98417ca53dd2dd085971d78dabfd99caaf1769ee
Xiaofeng Guo <xguo@vectoratomic.com>
2024-12-23 16:32:45 -0800

    Use the way postcard did, build passed.

--- ferox/src/proto/ascii.rs
+++ ferox/src/proto/ascii.rs
@@ -1,19 +1,25 @@
 use deser::AsciiDeserializer;
-use heapless::String;
+use heapless::Vec;
 use ser::AsciiSerializer;
 use serde::{Deserialize, Serialize};
+
 use crate::{common::MAX_STRING_SIZE, proto::error::Error as FeroxError};
 
-pub mod ser;
 pub mod deser;
+pub mod ser;
+pub mod vec;
 
-pub fn to_string<T>(value: &T) -> Result<String<MAX_STRING_SIZE>, FeroxError>
+pub fn to_string<T>(value: &T) -> Result<Vec<u8, MAX_STRING_SIZE>, FeroxError>
 where
     T: Serialize,
 {
-    let mut serializer = AsciiSerializer::new();
+    let mut serializer = AsciiSerializer::new(vec::FeroxVec::<MAX_STRING_SIZE>::new());
     value.serialize(&mut serializer)?;
-    Ok(serializer.into_inner())
+    let t = serializer
+        .finalize()
+        .release()
+        .map_err(|_| FeroxError::BufferOverflow)?;
+    Ok(t)
 }
 
 pub fn from_str<'a, T>(input: &'a str) -> Result<T, serde::de::value::Error>
@@ -22,56 +28,4 @@ where
 {
     let deserializer = AsciiDeserializer::new(input);
     T::deserialize(deserializer)
-}
-
-#[cfg(test)]
-mod tests {
-    use defmt_or_log::info;
-
-    use crate::proto::ferox::{AnotherEnum, FeroxRequest};
-    use super::*;
-
-    #[test]
-    fn test_serialize_version() {
-        env_logger::builder().is_test(true).try_init().unwrap();
-        info!("test_serialize_version: INFO");
-        let request = FeroxRequest::Version;
-        let serialized = to_string(&request).unwrap();
-        assert_eq!(serialized, "ver");
-    }
-
-    #[test]
-    fn test_another_serialize_version() {
-        env_logger::builder().is_test(true).try_init().unwrap();
-        info!("test_another_serialize_version: INFO");
-        let request = FeroxRequest::Another(AnotherEnum::Version);
-        let serialized = to_string(&request).unwrap();
-        assert_eq!(serialized, "another ver");
-    }
-
-    #[test]
-    fn test_deserialize_version() {
-        let input = "Version";
-        let deserialized: FeroxRequest = from_str(input).unwrap();
-        assert_eq!(deserialized, FeroxRequest::Version);
-    }
-
-    #[test]
-    fn test_serialize_forward() {
-        env_logger::builder().is_test(true).try_init().unwrap();
-        info!("test_serialize_forward: INFO");
-        let request = FeroxRequest::SmcForward { data: b"bia?" };
-        let serialized = to_string(&request).unwrap();
-        assert_eq!(serialized, "smc bia?");
-    }
-
-    #[test]
-    fn test_deserialize_forward() {
-        let input = "SmcForward example";
-        let deserialized: FeroxRequest = from_str(input).unwrap();
-        assert_eq!(
-            deserialized,
-            FeroxRequest::SmcForward { data: b"example" }
-        );
-    }
 }
--- ferox/src/proto/ascii/ser.rs
+++ ferox/src/proto/ascii/ser.rs
@@ -1,36 +1,59 @@
 use defmt_or_log::info;
-use heapless::String;
-use serde::{ser::{SerializeMap, SerializeSeq, SerializeStruct, SerializeStructVariant, SerializeTuple, SerializeTupleStruct, SerializeTupleVariant}, Serialize, Serializer};
+use postcard::ser_flavors::Flavor;
+use serde::{
+    ser::{
+        SerializeMap, SerializeSeq, SerializeStruct, SerializeStructVariant, SerializeTuple,
+        SerializeTupleStruct, SerializeTupleVariant,
+    },
+    Serialize, Serializer,
+};
 
-use crate::{common::MAX_STRING_SIZE, proto::error::Error as FeroxError};
+use crate::{proto::error::Error as FeroxError};
 
-pub struct AsciiSerializer {
-    buffer: String<MAX_STRING_SIZE>,
+pub struct AsciiSerializer<F: Flavor> {
+    buffer: F,
 }
 
-impl AsciiSerializer {
-    pub fn new() -> Self {
+impl<F: Flavor> AsciiSerializer<F> {
+    pub fn new(buffer: F) -> Self {
         Self {
-            buffer: String::new(),
+            buffer,
         }
     }
 
-    pub fn into_inner(self) -> String<MAX_STRING_SIZE> {
+    fn try_extend(&mut self, data: &[u8]) -> Result<(), FeroxError> {
+        self.buffer.try_extend(data).map_err(|_| FeroxError::BufferOverflow)
+    }
+
+    pub fn finalize(self) -> F {
         self.buffer
     }
 }
 
-pub struct ObjSerializer<'a> {
-    _buffer: &'a mut AsciiSerializer,
+impl<F: Flavor> SerializeSeq for &mut AsciiSerializer<F> {
+    type Ok = ();
+    type Error = FeroxError;
+
+    fn serialize_element<T>(&mut self, value: &T) -> Result<(), Self::Error>
+    where
+        T: ?Sized + Serialize,
+    {
+        todo!()
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
 }
 
-impl<'a> SerializeSeq for ObjSerializer<'a> {
+impl<F: Flavor> SerializeTuple for &mut AsciiSerializer<F> {
     type Ok = ();
     type Error = FeroxError;
 
     fn serialize_element<T>(&mut self, _value: &T) -> Result<(), Self::Error>
     where
-        T: ?Sized + Serialize {
+        T: ?Sized + Serialize,
+    {
         todo!()
     }
 
@@ -39,43 +62,29 @@ impl<'a> SerializeSeq for ObjSerializer<'a> {
     }
 }
 
-impl<'a> SerializeTuple for ObjSerializer<'a> {
-    type Ok = ();
-    type Error = FeroxError;
-
-    fn serialize_element<T>(&mut self, _value: &T) -> Result<(), Self::Error>
-    where
-        T: ?Sized + Serialize {
-        todo!()
-    }
-
-    fn end(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-}
-
-impl<'a> SerializeTupleStruct for ObjSerializer<'a> {
-    type Ok = ();
-    type Error = FeroxError;
-    
-    fn serialize_field<T>(&mut self, _value: &T) -> Result<(), Self::Error>
-    where
-        T: ?Sized + Serialize {
-        todo!()
-    }
-    
-    fn end(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-}
-
-impl<'a> SerializeTupleVariant for ObjSerializer<'a> {
+impl<F: Flavor> SerializeTupleStruct for &mut AsciiSerializer<F> {
     type Ok = ();
     type Error = FeroxError;
 
     fn serialize_field<T>(&mut self, _value: &T) -> Result<(), Self::Error>
     where
-        T: ?Sized + Serialize {
+        T: ?Sized + Serialize,
+    {
+        todo!()
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+}
+impl<F: Flavor> SerializeTupleVariant for &mut AsciiSerializer<F> {
+    type Ok = ();
+    type Error = FeroxError;
+
+    fn serialize_field<T>(&mut self, _value: &T) -> Result<(), Self::Error>
+    where
+        T: ?Sized + Serialize,
+    {
         todo!()
     }
 
@@ -84,19 +93,21 @@ impl<'a> SerializeTupleVariant for ObjSerializer<'a> {
     }
 }
 
-impl<'a> SerializeMap for ObjSerializer<'a> {
+impl<F: Flavor> SerializeMap for &mut AsciiSerializer<F> {
     type Ok = ();
     type Error = FeroxError;
 
     fn serialize_key<T>(&mut self, _key: &T) -> Result<(), Self::Error>
     where
-        T: ?Sized + Serialize {
+        T: ?Sized + Serialize,
+    {
         todo!()
     }
 
     fn serialize_value<T>(&mut self, _value: &T) -> Result<(), Self::Error>
     where
-        T: ?Sized + Serialize {
+        T: ?Sized + Serialize,
+    {
         todo!()
     }
 
@@ -105,13 +116,14 @@ impl<'a> SerializeMap for ObjSerializer<'a> {
     }
 }
 
-impl<'a> SerializeStruct for ObjSerializer<'a> {
+impl<F: Flavor> SerializeStruct for &mut AsciiSerializer<F> {
     type Ok = ();
     type Error = FeroxError;
 
     fn serialize_field<T>(&mut self, _key: &'static str, _value: &T) -> Result<(), Self::Error>
     where
-        T: ?Sized + Serialize {
+        T: ?Sized + Serialize,
+    {
         todo!()
     }
 
@@ -120,13 +132,14 @@ impl<'a> SerializeStruct for ObjSerializer<'a> {
     }
 }
 
-impl<'a> SerializeStructVariant for ObjSerializer<'a> {
+impl<F: Flavor> SerializeStructVariant for &mut AsciiSerializer<F> {
     type Ok = ();
     type Error = FeroxError;
 
     fn serialize_field<T>(&mut self, _key: &'static str, _value: &T) -> Result<(), Self::Error>
     where
-        T: ?Sized + Serialize {
+        T: ?Sized + Serialize,
+    {
         todo!()
     }
 
@@ -137,24 +150,28 @@ impl<'a> SerializeStructVariant for ObjSerializer<'a> {
 
 // TODO(xguo): Remove the allow below.
 #[allow(unused_variables)]
-impl<'a> Serializer for &'a mut AsciiSerializer {
+impl<F: Flavor> Serializer for &mut AsciiSerializer<F> {
     type Ok = ();
     type Error = FeroxError;
 
-    type SerializeSeq = ObjSerializer<'a>;
-    type SerializeTuple = ObjSerializer<'a>;
-    type SerializeTupleStruct = ObjSerializer<'a>;
-    type SerializeTupleVariant = ObjSerializer<'a>;
-    type SerializeMap = ObjSerializer<'a>;
-    type SerializeStruct = ObjSerializer<'a>;
-    type SerializeStructVariant = ObjSerializer<'a>;
+    type SerializeSeq = Self;
+    type SerializeTuple = Self;
+    type SerializeTupleStruct = Self;
+    type SerializeTupleVariant = Self;
+    type SerializeMap = Self;
+    type SerializeStruct = Self;
+    type SerializeStructVariant = Self;
 
     fn serialize_str(self, value: &str) -> Result<Self::Ok, Self::Error> {
         info!("Serializing string: {}", value);
-        self.buffer.push_str(value).map_err(|_| FeroxError::BufferOverflow)
+        self.try_extend(value.as_bytes())
     }
 
-    fn serialize_newtype_struct<T>(self, _: &'static str, value: &T) -> Result<Self::Ok, Self::Error>
+    fn serialize_newtype_struct<T>(
+        self,
+        _: &'static str,
+        value: &T,
+    ) -> Result<Self::Ok, Self::Error>
     where
         T: ?Sized + Serialize,
     {
@@ -168,11 +185,14 @@ impl<'a> Serializer for &'a mut AsciiSerializer {
         variant_index: u32,
         _variant: &'static str,
     ) -> Result<Self::Ok, Self::Error> {
-        info!("Serializing unit variant: name = {}, variant_index = {}, variant = {}", _name, variant_index, _variant);
+        info!(
+            "Serializing unit variant: name = {}, variant_index = {}, variant = {}",
+            _name, variant_index, _variant
+        );
 
         self.serialize_str(&_variant)
     }
-    
+
     fn serialize_newtype_variant<T>(
         self,
         _: &'static str,
@@ -192,81 +212,82 @@ impl<'a> Serializer for &'a mut AsciiSerializer {
     fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_some<T>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
-        T: ?Sized + Serialize {
+        T: ?Sized + Serialize,
+    {
         todo!()
     }
-    
+
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_unit_struct(self, name: &'static str) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_tuple_struct(
         self,
         name: &'static str,
@@ -274,7 +295,7 @@ impl<'a> Serializer for &'a mut AsciiSerializer {
     ) -> Result<Self::SerializeTupleStruct, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_tuple_variant(
         self,
         name: &'static str,
@@ -284,11 +305,11 @@ impl<'a> Serializer for &'a mut AsciiSerializer {
     ) -> Result<Self::SerializeTupleVariant, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_struct(
         self,
         name: &'static str,
@@ -296,7 +317,7 @@ impl<'a> Serializer for &'a mut AsciiSerializer {
     ) -> Result<Self::SerializeStruct, Self::Error> {
         todo!()
     }
-    
+
     fn serialize_struct_variant(
         self,
         name: &'static str,
@@ -304,14 +325,159 @@ impl<'a> Serializer for &'a mut AsciiSerializer {
         variant: &'static str,
         len: usize,
     ) -> Result<Self::SerializeStructVariant, Self::Error> {
-        info!("Serializing struct variant: name = {}, variant_index = {}, variant = {}, len = {}", name, variant_index, variant, len);
+        info!(
+            "Serializing struct variant: name = {}, variant_index = {}, variant = {}, len = {}",
+            name, variant_index, variant, len
+        );
         // TODO(xguo): Implement this.
         Err(FeroxError::PlaceHolder)
     }
-    
+
     fn collect_str<T>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
-        T: ?Sized + core::fmt::Display {
+        T: ?Sized + core::fmt::Display,
+    {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use env_logger;
+    use serde::{Deserialize, Serialize};
+
+    use super::*;
+    use crate::proto::ascii::to_string;
+
+    #[derive(Serialize, Deserialize, Debug)]
+    enum TestReq<'a> {
+        #[serde(rename = "ver")]
+        Version,
+
+        #[serde(rename = "varint")]
+        VarInt(Option<i32>),
+
+        #[serde(rename = "varfloat")]
+        VarFloat(Option<f32>),
+
+        #[serde(rename = "varbool")]
+        VarBool(Option<bool>),
+
+        #[serde(rename = "varbytes")]
+        VarBytes(Option<&'a [u8]>),
+
+        #[serde(rename = "smc")]
+        SmcForward { request: &'a [u8] },
+
+        #[serde(rename = "recur")]
+        Recursive(RecursiveEnum),
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    enum RecursiveEnum {
+        #[serde(rename = "ver")]
+        Version,
+    }
+
+    #[test]
+    fn test_serialize_version() {
+        env_logger::builder().is_test(true).try_init().unwrap();
+        info!("test_serialize_failure: INFO");
+
+        assert_eq!(to_string(&TestReq::Version).unwrap(), "ver");
+    }
+    #[test]
+    fn test_serialize_varint_some() {
+        env_logger::builder().is_test(true).try_init().unwrap();
+        info!("test_serialize_varint_some: INFO");
+
+        assert_eq!(to_string(&TestReq::VarInt(Some(42))).unwrap(), "varint 42");
+    }
+
+    #[test]
+    fn test_serialize_varint_none() {
+        env_logger::builder().is_test(true).try_init().unwrap();
+        info!("test_serialize_varint_none: INFO");
+
+        assert_eq!(to_string(&TestReq::VarInt(None)).unwrap(), "varint?");
+    }
+
+    #[test]
+    fn test_serialize_varfloat_some() {
+        env_logger::builder().is_test(true).try_init().unwrap();
+        info!("test_serialize_varfloat_some: INFO");
+
+        assert_eq!(
+            to_string(&TestReq::VarFloat(Some(3.14))).unwrap(),
+            "varfloat 3.14"
+        );
+    }
+
+    #[test]
+    fn test_serialize_varfloat_none() {
+        env_logger::builder().is_test(true).try_init().unwrap();
+        info!("test_serialize_varfloat_none: INFO");
+
+        assert_eq!(to_string(&TestReq::VarFloat(None)).unwrap(), "varfloat?");
+    }
+
+    #[test]
+    fn test_serialize_varbool_some() {
+        env_logger::builder().is_test(true).try_init().unwrap();
+        info!("test_serialize_varbool_some: INFO");
+
+        assert_eq!(
+            to_string(&TestReq::VarBool(Some(true))).unwrap(),
+            "varbool true"
+        );
+    }
+
+    #[test]
+    fn test_serialize_varbool_none() {
+        env_logger::builder().is_test(true).try_init().unwrap();
+        info!("test_serialize_varbool_none: INFO");
+
+        assert_eq!(to_string(&TestReq::VarBool(None)).unwrap(), "varbool?");
+    }
+
+    #[test]
+    fn test_serialize_varbytes_some() {
+        env_logger::builder().is_test(true).try_init().unwrap();
+        info!("test_serialize_varbytes_some: INFO");
+
+        assert_eq!(
+            to_string(&TestReq::VarBytes(Some(b"hello"))).unwrap(),
+            "varbytes hello"
+        );
+    }
+
+    #[test]
+    fn test_serialize_varbytes_none() {
+        env_logger::builder().is_test(true).try_init().unwrap();
+        info!("test_serialize_varbytes_none: INFO");
+
+        assert_eq!(to_string(&TestReq::VarBytes(None)).unwrap(), "varbytes?");
+    }
+
+    #[test]
+    fn test_serialize_smcforward() {
+        env_logger::builder().is_test(true).try_init().unwrap();
+        info!("test_serialize_smcforward: INFO");
+
+        assert_eq!(
+            to_string(&TestReq::SmcForward { request: b"bia?" }).unwrap(),
+            "smc bia?"
+        );
+    }
+
+    #[test]
+    fn test_serialize_recursive() {
+        env_logger::builder().is_test(true).try_init().unwrap();
+        info!("test_serialize_recursive: INFO");
+
+        assert_eq!(
+            to_string(&TestReq::Recursive(RecursiveEnum::Version)).unwrap(),
+            "recur ver"
+        );
     }
 }
